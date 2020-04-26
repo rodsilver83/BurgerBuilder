54b5a1be41a96b7238c599dec67a8bd749fa6ad5
Rodrigo Soria <rodsilver83@hotmail.com>
2020-04-26 10:25:12 -0400

    Updates

---
+++ precache-manifest.378494846aeaae05c2e4216446721ae9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b863d851b9e01cb282681449e54942",
+    "url": "/BurguerBuilder/index.html"
+  },
+  {
+    "revision": "982c880fb3c6f75dfdf1",
+    "url": "/BurguerBuilder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "221eb9e74cbdf88f62ed",
+    "url": "/BurguerBuilder/static/js/2.161d64f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BurguerBuilder/static/js/2.161d64f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "982c880fb3c6f75dfdf1",
+    "url": "/BurguerBuilder/static/js/main.ca47881e.chunk.js"
+  },
+  {
+    "revision": "955c1bfa9cedbc2039ae",
+    "url": "/BurguerBuilder/static/js/runtime-main.d748c5e0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BurguerBuilder/static/media/logo.5d5d9eef.svg"
+  }
+]);
